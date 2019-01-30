954181ebffeda40266b19f3d71333804241331c1
JamesonThai <jameson.k.thai@gmail.com>
2019-01-29 20:43:21 -0800

    updated temp matches and datasets

--- getFiles.py
+++ getFiles.py
@@ -9,12 +9,12 @@
 import requests, json, time
 
 """
-	param: region, the corresponding region that is being called
-	param: accID, the accountID of the summoner account in question
-	param: API_KEY, my API keyused for development, for publishing purposes this has been removed due to 
+	param region:	the corresponding region that is being called
+	param accID:	the accountID of the summoner account in question
+	param API_KEY:	my API keyused for development, for publishing purposes this has been removed due to 
 			riot games policies, if you want to create your own you need to make your own account and 
 			request from there
-	param: version , the version called in regards to Riot Games API calls 
+	param version:	the version called in regards to Riot Games API calls 
 	Return: 
 		returns the extracted JSON file 
 """
@@ -24,18 +24,39 @@ def getPlayerMatches(region, accID, API_KEY, version):
 	response = requests.get(URL)
 	return response.json()
 
+"""
+	param region:	the corresponding region that is being called
+	param matchID: 	the match ID
+	param API_KEY:	our API KEY BEING CALLED blocked out for certain reasons...
+	param version:	version of current patch
+	return
+		returns extracted json file
+"""
 def getPlayerMatch(region, matchID, API_KEY, version):
 	URL = "https://" + region + ".api.riotgames.com/lol/match/" + version + "/matches/" + str(matchID) + "?api_key=" + API_KEY
 	response = requests.get(URL)
 	return response.json()
 
-# Rename such that it is labeled by gameID
+"""
+	param region:	the corresponding region that is being called
+	param matchID: 	the match ID
+	param API_KEY:	our API KEY BEING CALLED blocked out for certain reasons...
+	param version:	version of current patch
+	return
+		returns extracted json file
+"""
 def getPlayerTimeline(region, matchID, API_KEY, version):
 	URL = "https://" + region + ".api.riotgames.com/lol/match/" + version + "/timelines/by-match/" + str(matchID) + "?api_key=" + API_KEY
 	response = requests.get(URL)
 	return response.json()
 
 """
+	param region:			the corresponding region that is being called
+	param summonerName: 	the summoner name being searched for his encrypted account ID
+	param API_KEY:			our API KEY BEING CALLED blocked out for certain reasons...
+	param version:			version of current patch
+	return
+		returns extracted json file
 """
 def getSummonerName(region, summonerName, API_KEY, version):
 	# Sample https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/RiotSchmick?api_key=<key>
@@ -66,6 +87,7 @@ def getProPlayers():
 	return proList
 
 """
+	returns list of seen games
 """
 def getSeenGames():
 	path = "data/seenGameIDs.txt"
@@ -77,6 +99,13 @@ def getSeenGames():
 	return seenGames
 
 """
+	param regions:			the corresponding regions that is being called
+	param proPlayerList: 	proPlayer list 
+	param API_KEY:			our API KEY BEING CALLED blocked out for certain reasons...
+	param version:			version of current patch
+	return
+		returns extracted json file
+
 	Return: In the format of a dictionary of PlayerID(SummonerName) and region as key value pairing 
 """
 def getPlayerAccId(proPlayerList, regions, API_KEY, version):
@@ -95,20 +124,11 @@ def getPlayerAccId(proPlayerList, regions, API_KEY, version):
 			time.sleep(.9)
 	return listOfAccounts
 
-def aggregateData(matches, summonerName, region, API_KEY, version):
-	# Need to get list of matches
-	matchID = matches[0]
-	match = getPlayerMatch(region, matchID, API_KEY, version)
-	matchTimeline = getPlayerTimeline(region, matchID, API_KEY, version)
-	matchName = "data/matches/" + str(matchID) + "match.json"
-	matchTimelineName = "data/matchTimelines/" + str(matchID) + "matchTimeline.json"
-	with open(matchName,"w") as outfile:
-		json.dump(match, outfile, sort_keys = True, indent = 4, ensure_ascii=False)
-
-	with open(matchTimelineName,"w") as outTime:
-		json.dump(matchTimeline, outTime, sort_keys = True, indent = 4, ensure_ascii=False)
-
 """
+	param playerAccIds: 	playerAccIds list 
+	param API_KEY:			our API KEY BEING CALLED blocked out for certain reasons...
+	param version:			version of current patch
+	param seenGames:		list of seen games for checking
 """
 def getAllOfPlayersMatches(playerAccIds, API_KEY, version, seenGames):
 	ListOfMatches = []
@@ -137,8 +157,8 @@ def getAllOfPlayersMatches(playerAccIds, API_KEY, version, seenGames):
 					out.write(str(matchID) + "\n")
 				out.close()
 			i += 1
-		time.sleep(.9)
-		# Don't unbreak this unless you want 400+ players * # of n games
+			time.sleep(.9)
+		# Don't unbreak this unless you want 400+ players * # of n games, or atleast for the time being
 		break
 
 """
--- main.py
+++ main.py
@@ -1,4 +1,42 @@
 """
 	the main runner that will call everything and execute/compile for data collection and preprocessing 
-	
+
+	Written and Documented by: Jameson Thai	
 """
+def main():
+	# Constants
+	API_KEY = "RGAPI-2809e3e8-1012-4674-b2ba-a88efa2924dd"
+	version = "v4"
+	regions = {
+		"NA"   : "na1",
+		"BR"   : "br1",
+		"EUNE" : "eun1",
+		"EUW"  : "euw1",
+		"JP"   : "jp1",
+		"KR"   : "kr",
+		"LAS"  : "la1",
+		"LAN"  : "la2",
+		"OCE"  : "oc1",
+		"TR"   : "tr1",
+		"RU"   : "ru",
+		"PBE"  : "pbe1",
+	}
+
+	# Call main functions from other files
+
+	proList = getProPlayers()
+	seenGames = getSeenGames()
+	# playerAccIds = getPlayerAccId(proList, regions, API_KEY, version)
+	# Temporarily for utility 
+	playerAccIds = {}
+	with open("data/tempAccs.txt", "r") as outfile:
+		for line in outfile:
+			temp = line[line.find("|") + 1:] 
+			region = temp[:temp.find("|")].strip()
+			accID = temp[temp.find("|") + 1 :].strip()
+			playerAccIds[accID] = region
+	outfile.close()
+	playerMatches = getAllOfPlayersMatches(playerAccIds, API_KEY, version, seenGames)
+
+if __name__ == "__main__":
+	main()
